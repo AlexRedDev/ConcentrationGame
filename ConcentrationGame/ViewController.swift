4ec00991bdf392a0c361b1cd550ba28bdac51090
Alex <garishnavi@gmail.com>
2021-05-10 23:20:45 +0300

    Creat Clear Project

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var unitPercent: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
