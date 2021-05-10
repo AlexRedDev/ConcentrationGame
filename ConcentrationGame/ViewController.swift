6c0eb53a31a42d70cd1ad410e0e354989b9ca6ec
Alex <garishnavi@gmail.com>
2021-05-11 00:08:31 +0300

    Add button Card and simple logic for this card

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var toucheLabel: UILabel!
+    @IBOutlet var buttonCollection: [UIButton]!
+    
+    private let emojiCollection = ["🦊", "🐹", "🐻", "🐸"];
+    private var touches = 0 {
+        didSet {
+            toucheLabel.text = "Touches: \(touches)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Customize ButtonCard
+        for button in buttonCollection {
+            button.layer.cornerRadius = 15
+        }
+        
     }
-
-
+    
+    func flipButton(emoji: String, button: UIButton) -> Void {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = .white
+        }
+    }
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojiCollection[buttonIndex], button: sender)
+        }
+    }
 }
 
